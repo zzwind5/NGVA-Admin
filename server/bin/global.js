f1e9b21b00c0acb7fd2ea0c112202a85409ca79a
zjie <zjie@aerohive.com>
2018-01-25 17:02:43 +0800

    service status enhancement

--- server/bin/global.js
+++ server/bin/global.js
@@ -2,4 +2,4 @@ global.$fs = require('fs');
 global.$path = require('path');
 global._ = require('lodash');
 global.__home = $path.join(__dirname, '../');
-global.$config = JSON.parse($fs.readFileSync(`${__home}/config.json`));
+global.$config = JSON.parse($fs.readFileSync(`${__home}config.json`));
